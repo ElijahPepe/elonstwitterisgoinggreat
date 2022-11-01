f4042e64880b564a3438df58b64fad743aa8b36e
Elijah Conners <business@elijahpepe.com>
2022-10-31 23:56:21 -0700

    fix: fix apostrophe issue in title
    Signed-off-by: Elijah Conners <business@elijahpepe.com>

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document(props) {
   return (
     <Html lang="en">
       <Head>
-        <title>Elon's Twitter is Going Great</title>
+        <title>Elon&apos;s Twitter is Going Great</title>
       </Head>
       <body className="bg-gray-50 dark:bg-gray-900 text-white dark:text-black">
         <Main />
