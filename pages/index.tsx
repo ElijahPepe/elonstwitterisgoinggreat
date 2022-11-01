005a5911d5cce240e682ea39123ea6604fdcbff1
Elijah Conners <business@elijahpepe.com>
2022-11-01 16:23:13 -0700

    fix: remove unnecessary const and fix padding
    Signed-off-by: Elijah Conners <business@elijahpepe.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,10 +5,9 @@ import { useTheme } from 'next-themes'
 /* eslint-disable react/no-unescaped-entities */
 export default function Home() {
   const { theme } = useTheme();
-  const fillTheme = theme === 'dark' ? '#fff' : '#000';
 
   return (
-    <div className="bg-gray-50 dark:bg-gray-900">
+    <div className="px-8 bg-gray-50 dark:bg-gray-900">
       <div className="flex flex-col justify-center items-start max-w-3xl pt-8 border-gray-200 dark:border-gray-700 mx-auto pb-16">
         <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-1 text-black dark:text-white">
           Elon's Twitter is going just great.
