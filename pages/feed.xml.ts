03c4490086d60f7c81103ad35603b0c0a7ef085d
Elijah Conners <business@elijahpepe.com>
2022-11-02 12:45:46 -0700

    feat: add RSS
    Signed-off-by: Elijah Conners <business@elijahpepe.com>

---
+++ pages/feed.xml.ts
@@ -0,0 +1,14 @@
+import { buildFeed } from "../lib/feed";
+
+const Feed = () => null;
+
+export const getServerSideProps = async ({ res }) => {
+  const feed = await buildFeed();
+
+  res.setHeader("Content-Type", "application/atom+xml;charset=UTF-8");
+  res.write(feed.rss2());
+  res.end();
+  return { props: {} }
+};
+
+export default Feed;
